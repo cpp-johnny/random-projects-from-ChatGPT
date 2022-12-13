db6890af8c42807836d77c5ed4bb2ca3ed6cbd0e
Boi <119715263+cpp-johnny@users.noreply.github.com>
2022-12-13 12:28:59 +0800

    Create econs-graph.py

---
+++ econs-graph.py
@@ -0,0 +1,16 @@
+import matplotlib.pyplot as plt
+
+# Set the x and y values for the graph
+x = [1, 2, 3, 4, 5]
+y = [100, 200, 300, 400, 500]
+
+# Create the line chart
+plt.plot(x, y)
+
+# Add labels and title
+plt.xlabel('Time')
+plt.ylabel('Values')
+plt.title('Econs Graph')
+
+# Show the graph
+plt.show()
