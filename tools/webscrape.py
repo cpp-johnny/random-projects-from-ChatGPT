ad41beddc00d9cfe3ae954fe9608c58054fc2b77
Johnny <119715263+cpp-johnny@users.noreply.github.com>
2022-12-28 23:39:24 +0800

    Update webscrape.py

--- tools/webscrape.py
+++ tools/webscrape.py
@@ -1,4 +1,4 @@
-// scraping data from https://advent-of-code.creator-spring.com/?
+# scraping data from https://advent-of-code.creator-spring.com/?
 
 import requests
 from bs4 import BeautifulSoup
@@ -16,3 +16,23 @@ soup = BeautifulSoup(html_content, "html.parser")
 links = soup.find_all("a")
 for link in links:
     print(link.get("href"))
+    
+    # Find all the div tags with the class "item"
+items = soup.find_all("div", class_="item")
+
+
+# scrape link of each item
+# Iterate over the items and extract the link
+for item in items:
+    # Find the link element within the item
+    link_element = item.find("a")
+    # Extract the link from the element
+    link = link_element.get("href")
+    print(link)
+    
+"""    
+This will find all the div tags with the class "item", and then extract the link from the a tag within each div.
+
+Keep in mind that the specific HTML tags and attributes you need to search for will depend on the structure of the website's HTML code. You may need to inspect the HTML code of the page to determine which tags and attributes contain the information you are interested in.
+
+"""
