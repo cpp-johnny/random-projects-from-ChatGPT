8ce1949e8d29c411c06229f72410cca21c3551a3
Johnny <119715263+cpp-johnny@users.noreply.github.com>
2022-12-30 20:34:39 +0800

    Create brain-cells.py

---
+++ graphs/brain-cells.py
@@ -0,0 +1,16 @@
+import matplotlib.pyplot as plt
+
+# Create some data points
+time = [0, 1, 2, 3, 4, 5, 6, 7, 8]
+brain_cells = [100, 75, 50, 25, 0, -25, -50, -75, -100]
+
+# Plot the graph
+plt.plot(time, brain_cells)
+
+# Add labels and title
+plt.xlabel('Time (hours)')
+plt.ylabel('Brain cells')
+plt.title('Brain cells over time')
+
+# Show the plot
+plt.show()
