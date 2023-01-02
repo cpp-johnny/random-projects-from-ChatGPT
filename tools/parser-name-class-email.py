030e0813066c016ffd30c667e5acac9f44004b6d
Johnny <119715263+cpp-johnny@users.noreply.github.com>
2023-01-02 18:56:45 +0800

    Create parser-name-class-email.py

---
+++ tools/parser-name-class-email.py
@@ -0,0 +1,34 @@
+def extract_info(filename):
+    # Initialize variables to store the name, class, and email
+    name = None
+    class_ = None
+    email = None
+    
+    # Open the file and read through it line by line
+    with open(filename, 'r') as f:
+        for line in f:
+            # Check if the line starts with "Name: "
+            if line.startswith('Name: '):
+                # Extract the name from the line
+                name = line.split(': ')[1].strip()
+            # Check if the line starts with "Class: "
+            elif line.startswith('Class: '):
+                # Extract the class from the line
+                class_ = line.split(': ')[1].strip()
+            # Check if the line starts with "Email: "
+            elif line.startswith('Email: '):
+                # Extract the email from the line
+                email = line.split(': ')[1].strip()
+    
+    # If no name was found, return an error message
+    if name is None:
+        return "Error: No name found in file."
+    # If no class was found, set class_ to [no class]
+    if class_ is None:
+        class_ = '[no class]'
+    # If no email was found, set email to [no email]
+    if email is None:
+        email = '[no email]'
+    
+    # Return the extracted information as a tuple
+    return (name, class_, email)
