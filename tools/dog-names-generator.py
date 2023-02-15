9cb73b43dd3000684bda5fcbcf7eb0eed9ad2a67
boi <119715263+cpp-johnny@users.noreply.github.com>
2023-02-15 10:37:38 +0800

    Create dog-names-generator.py

---
+++ tools/dog-names-generator.py
@@ -0,0 +1,10 @@
+import random
+
+# List of possible dog names
+dog_names = ['Buddy', 'Max', 'Bailey', 'Charlie', 'Rocky', 'Cooper', 'Bear', 'Duke', 'Toby', 'Jack', 'Oscar', 'Teddy', 'Milo', 'Winston', 'Rufus', 'Gizmo', 'Murphy', 'Apollo', 'Harley', 'Finn']
+
+# Generate a random dog name
+random_dog_name = random.choice(dog_names)
+
+# Print the random dog name
+print("Your dog's name is:", random_dog_name)
